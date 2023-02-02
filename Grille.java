e1fd2607bbe339ab06ed6731b1ee72aada27e696
MERCIER Loris <loris.mercier@etu.univ-lyon1.fr>
2023-02-02 11:42:41 +0100

    TabCurrent et Tabfinish

--- Grille.java
+++ Grille.java
@@ -1,9 +1,19 @@
 public class Grille {
     private int sizeX, sizeY;
+    private int [][] tabCurrent;
+    private int [][] tabTarget;
+    private boolean finished;
+
+    public boolean isFinished() {
+        return finished;
+    }
 
     Grille(final int x, final int y) {
         sizeX = x;
         sizeY = y;
+        tabCurrent = new int[x][y];
+        tabTarget = new int[x][y];
+        finished = false;
     }
 
     public int getSizeX(){
@@ -12,5 +22,10 @@ public class Grille {
 
     public int getSizeY() {
         return sizeY;
+    }
+
+    public void move(Position p1, Position p2) {
+        tabCurrent[p2.X()][p2.Y()] = tabCurrent[p1.X()][p1.Y()];
+        tabCurrent[p1.X()][p1.Y()] = -1;
     }
 }
